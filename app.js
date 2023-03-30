97d8437537cf5b6b76620a2cc0b244c5831f0254
Peter Laughlin <pwlaughlin@gmail.com>
2023-03-29 23:40:47 -0500

    do it to it

--- app.js
+++ app.js
@@ -1,48 +1,60 @@
 /****State */
-const state = {};
+const state = {
+board: [
+['', '', ''],
+['', '', ''],
+['', '', '']
+],
+players:  [ '', ''],
+score : [0, 0],
+};
 
-state.board = ['this is the board'];
+
 console.log(state)
 
-
-// dom selectors
+//*************************** */ dom selectors
 
 //body element
 const body =  document.querySelector( 'body' )
 
 ///Title Element
 const titleH1= document.createElement( 'h1' )
-
 titleH1.id = 'title';
 titleH1.innerText = 'This is Tick Tack Toe';
-
-
 body.appendChild( titleH1)
 
 // Main element
 const main = document.createElement('main');
+const renderBoard = () => {
 main.id = 'board';
-
-for(let i =0; i <9;  i ++){
+for(let i =0; i < 9;  i ++){
     const div = document.createElement ("div");
     div.className = 'box'
-    main.appendChild(div);
+    div.innerText = state.board[Math.floor(i / 3)][i % 3];
+     main.appendChild(div);
 }
-
+}
 body.appendChild(main);
+renderBoard();
+
 
 //player element
 const playerElem = document.createElement('p');
-
+const renderPlayer = () => {
 playerElem.id = 'playerNameElem';
-
 let playerElemHTML = '<input placeholder="Enter name Player 1" /><input placeholder="Enter name Player 2" /> <button>Start Game</button>';
-
 playerElem.innerHTML = playerElemHTML;
+body.appendChild(playerElem);}
+renderPlayer();
 
-body.appendChild(playerElem);
 
 
+
+function render(){
+    renderPlayer();
+    renderBoard();
+}
+    console.log('state is', state);
 
 
 
